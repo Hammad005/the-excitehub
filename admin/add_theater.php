eca9efbf556ed91bc6170de18992157836cbe10f
Hammad <127682041+Hammad005@users.noreply.github.com>
2024-09-16 15:21:28 +0500

    Update add_theater.php

--- admin/add_theater.php
+++ admin/add_theater.php
@@ -82,9 +82,9 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
 				$result=mysqli_query($conn,$sql);
 				if($result){
 					$showAlert=true;
-          $f_name = 'C:\xampp\htdocs\TheExciteHub/'.$file_name .'.php';
+          $f_name = '../'.$file_name .'.php';
                     $fp = fopen($f_name, 'w');
-                    $content = file_get_contents('C:\xampp\htdocs\TheExciteHub\theater_layout.php');
+                    $content = file_get_contents('../theater_layout.php');
                     fwrite($fp, $content);
                     fclose($fp);
 				}
